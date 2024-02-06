4e8d771f9ffeb7abe09c8dc121e7fec379ec8262
Paul <152566965+plsaballo08@users.noreply.github.com>
2024-02-06 13:50:52 +0800

    Update form.php

--- form.php
+++ form.php
@@ -120,8 +120,10 @@
   //$password = "";
   //$dbname = "myDB";
 
- 
-
+  $servername = "localhost";
+  $username = "webprogmi222_sf221";
+  $password = "xE*Y2nleNVvZm[!!";
+  $dbname = "webprogmi222_sf221";
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
