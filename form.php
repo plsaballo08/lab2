9ed78c52c1cc74f8b91b46cb163bca01589b4207
Paul <152566965+plsaballo08@users.noreply.github.com>
2024-02-06 13:44:57 +0800

    Update form.php

--- form.php
+++ form.php
@@ -120,10 +120,7 @@
   //$password = "";
   //$dbname = "myDB";
 
-  $servername = "localhost";
-  $username = "webprogmi222_sf221";
-  $password = "xE*Y2nleNVvZm[!!";
-  $dbname = "webprogmi222_sf221";
+ 
 
 
   // Create connection
