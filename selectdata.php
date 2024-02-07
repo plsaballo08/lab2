97e00d49742e46f6d98d07937023f2d31f924d31
Paul <152566965+plsaballo08@users.noreply.github.com>
2024-02-07 13:23:51 +0800

    Update selectdata.php

--- selectdata.php
+++ selectdata.php
@@ -6,11 +6,20 @@ $password = "xE*Y2nleNVvZm[!!";
 $dbname = "webprogmi222_sf221";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "INSERT INTO pcsaballo3_myguests (name, email, website, comment, gender)
+VALUES ('John', 'john@example.com', 'youtube.com', 'test', 'male')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
