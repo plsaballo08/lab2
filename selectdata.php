852a322a53ce24b777345aa87f04eeb8320428bf
Paul Lawrence Saballo <pcsaballo3@student.apc.edu.ph>
2024-02-07 14:15:37 +0800

    Update selectdata.php

--- selectdata.php
+++ selectdata.php
@@ -1,4 +1,11 @@
 <?php
+// For Xampp Localhost
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+// For socitcloud
 $servername = "localhost";
 $username = "webprogmi222_sf221";
 $password = "xE*Y2nleNVvZm[!!";
@@ -11,13 +18,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, gender FROM pcsaballo3_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM wddizon_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " <br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. 
+	" ". $row["website"]." ". $row["comment"]." ". $row["gender"]. "<br>";
   }
 } else {
   echo "0 results";
